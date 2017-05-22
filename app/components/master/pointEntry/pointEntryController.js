ad4092e1d48b02e90e80874aa5a932a5ed5d96b8
Htet Aung <mbcrnddept@gmail.com>
2017-05-22 14:45:18 +0630

    Data finding by id

--- app/components/master/pointEntry/pointEntryController.js
+++ app/components/master/pointEntry/pointEntryController.js
@@ -12,23 +12,32 @@ angular.module('hrApp').controller('pointEntryController', function ($scope, $ht
 
     $scope.add = function () {
         var l_point = angular.copy($scope.point);
-        l_point.pointId = ++pointKey;
+        l_point.key = ++pointKey;
         $scope.points.push(l_point);
         $scope.point.pointId = "";
         $scope.point.desc = "";
 
         alert($scope.points.length);
-    }
+    };
 
     $scope.remove = function (index) {
         $scope.points.splice(index, 1);
-    }
+    };
+
+    $scope.find = function (id) {
+        $scope.chosen = $scope.points.filter(function (val) {
+            return val.pointId == id;
+        });
+        if ($scope.chosen.length > 0) {
+            $scope.point.desc = $scope.chosen[0].desc;
+        }
+    };
 
     $scope.getData = function () {
-        $http.post('http://localhost:61884/HolaWs.asmx/GetAllHolas')
-            .then(function (response) {
-                $scope.holas = response.data;
-                alert($scope.holas);
-            })
+        // $http.post('http://localhost:61884/HolaWs.asmx/GetAllHolas')
+        //     .then(function (response) {
+        //         $scope.holas = response.data;
+        //         alert($scope.holas);
+        //     })
     };
 });
